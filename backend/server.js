c742f5aabf248a88cf188b1b16a01941377a5ee8
ArihantSrivastava2225/ <arihantsrivastava71011@gmail.com>
2025-07-29 16:56:41 +0530

    using multer's memory storage instead of disk storage

--- backend/server.js
+++ backend/server.js
@@ -12,20 +12,23 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // <-- Ensure JSON parsing for incoming requests
 
-//Multer setup for storing images
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const uploadDir = './uploads';
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir);
-        }
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueName = Date.now() + '-' + file.originalname.replace(/\s+/g, '_');
-        cb(null, uniqueName);
-    }
-});
+//Multer setup if you want storing images
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         const uploadDir = './uploads';
+//         if (!fs.existsSync(uploadDir)) {
+//             fs.mkdirSync(uploadDir);
+//         }
+//         cb(null, uploadDir);
+//     },
+//     filename: (req, file, cb) => {
+//         const uniqueName = Date.now() + '-' + file.originalname.replace(/\s+/g, '_');
+//         cb(null, uniqueName);
+//     }
+// });
+
+// Use memory storage
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
 
